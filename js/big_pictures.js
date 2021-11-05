d90ddd4530e98fe231cf386cf13817ff36420ee2
Валерий <valera.ch95@gmail.com>
2021-11-05 20:01:14 +0300

    Выполняю дом задание

--- js/big_pictures.js
+++ js/big_pictures.js
@@ -5,27 +5,43 @@ const createBigPicture = function(data) {
   const likesBigPicture = bigPicture.querySelector('.likes-count');
   const commentsBigPicture = bigPicture.querySelector('.comments-count');
   const descriptionBigPicture = bigPicture.querySelector('.social__caption');
+  const commentCount = bigPicture.querySelector('.social__comment-count');
+  const commentsLoader = bigPicture.querySelector('.comments-loader');
+  const body = document.querySelector('body');
   bigPicture.classList.remove('hidden');
+  commentCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+  body.classList.add('modal-open');
+  const onClosingWindow = function () {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+  };
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      onClosingWindow();
+    }
+  });
+  pictureCancel.addEventListener('click', () => onClosingWindow());
   urlBigPicture.src = data.url;
   likesBigPicture.textContent = data.likes;
   commentsBigPicture.textContent = data.comments.length;
   descriptionBigPicture.textContent = data.description;
-  pictureCancel.addEventListener('click', () => bigPicture.classList.add('hidden'));
-  const createComment = function () {
-    const comment = document.querySelector('.social__comment');
+  const createComment = function (i) {
+    const comment = document.querySelector('#social__comment').content;
     const clonedElement = comment.cloneNode(true);
     const commentAvatar = clonedElement.querySelector('.social__picture');
-    //const altName = clonedElement.querySelector('.');
+    const textComment = clonedElement.querySelector('.social__text');
     const dataObject = data.comments[i];
-    //const  = ;
-    //const  = ;
-    //commentAvatar.src = ;
-    commentsPicture.textContent = commmentsObject;
+    textComment.textContent = dataObject.message;
+    commentAvatar.setAttribute('alt', dataObject.name);
+    commentAvatar.src = dataObject.avatar;
     return clonedElement;
-  }
-
+  };
+  const comments = document.querySelector('.social__comments');
+  comments.textContent = '';
   for (let i = 0; i < data.comments.length; i++) {
-
+    const newComment = createComment(i);
+    comments.appendChild(newComment);
   }
 };
 
